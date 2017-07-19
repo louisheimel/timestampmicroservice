5919781bd928c2d15c8c2b47b66b947801c2f7a6
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2017-07-19 11:27:32 -0500

    add a comment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+// add a comment
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.get('/',(req, res) => { res.redirect('/static/index.html'); });
 app.get('/:query', function(req, res) {
